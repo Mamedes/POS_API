df2f132f2a6005b07ad8e4a46a177c545487be73
Mamedes <mamedeser@gmail.com>
2020-11-09 20:03:59 -0400

    first commit

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
+import AppError from '../errors/AppError';
 
 import authConfig from '../config/auth';
 
@@ -17,7 +18,7 @@ export default function ensureAuthenticated(
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
-    throw new Error('JWT token is missing');
+    throw new AppError('JWT token is missing', 401);
   }
 
   // bearer
@@ -26,7 +27,7 @@ export default function ensureAuthenticated(
 
   try {
     const decoded = verify(token, authConfig.jwt.secret);
-    /* Hack do Typescript */
+
     const { sub } = decoded as TokenPayload;
 
     request.user = {
@@ -35,6 +36,6 @@ export default function ensureAuthenticated(
 
     return next();
   } catch {
-    throw new Error('Invalid JWT Token');
+    throw new AppError('Invalid JWT Token', 401);
   }
 }
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -12,7 +12,6 @@ const appointmentsRouter = Router();
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.get('/', async (request, response) => {
-  console.log(request.user);
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
   const appointments = await appointmentsRepository.find();
 
@@ -20,21 +19,17 @@ appointmentsRouter.get('/', async (request, response) => {
 });
 
 appointmentsRouter.post('/', async (request, response) => {
-  try {
-    const { provider_id, date } = request.body;
+  const { provider_id, date } = request.body;
 
-    const parseDate = parseISO(date);
+  const parseDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService();
 
-    const appointments = await createAppointment.execute({
-      date: parseDate,
-      provider_id,
-    });
-    return response.json(appointments);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  const appointments = await createAppointment.execute({
+    date: parseDate,
+    provider_id,
+  });
+  return response.json(appointments);
 });
 
 export default appointmentsRouter;
